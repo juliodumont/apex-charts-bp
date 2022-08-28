3ed1a2ccc6a042d9d0e93c06926bc6a325563a3a
juliodumont <julioef.dumont@gmail.com>
2022-08-28 11:32:06 -0300

    Creating filters

--- frontend/src/components/Filter/Filter.tsx
+++ frontend/src/components/Filter/Filter.tsx
@@ -1,7 +1,36 @@
+import 'flatpickr/dist/themes/material_green.css';
+import flatpickrLib from 'flatpickr';
+import { Portuguese } from 'flatpickr/dist/l10n/pt';
+import FlatPicker from 'react-flatpickr';
 import './Filter.scss';
 
+flatpickrLib.localize(Portuguese);
+
 const Filter = () => {
-  return <div className="filter-container base-card">Filter</div>;
+  const onChangedate = (dates: Date[]) => {
+    console.log(dates);
+  };
+
+  return (
+    <div className="filter-container base-card">
+      <FlatPicker
+        options={{
+          mode: 'range',
+          dateFormat: 'd/m/Y',
+          showMonths: 2
+        }}
+        className="filter-input"
+        onChange={onChangedate}
+        placeholder="Selecione um período"
+      />
+      <select className="filter-input">
+        <option value="">Selecione um gênero</option>
+        <option value="MALE">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outro</option>
+      </select>
+    </div>
+  );
 };
 
 export default Filter;
