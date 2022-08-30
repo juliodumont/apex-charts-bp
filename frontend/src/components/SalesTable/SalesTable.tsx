7266b12fc1bec974db72a9ab9ff63534d3390321
juliodumont <julioef.dumont@gmail.com>
2022-08-30 11:28:47 -0300

    Backend integration complete

--- frontend/src/components/SalesTable/SalesTable.tsx
+++ frontend/src/components/SalesTable/SalesTable.tsx
@@ -1,8 +1,32 @@
+import { useEffect, useMemo, useState } from 'react';
+import { FilterData, Sale, SalesResponse } from '../../types';
+import { formatDate, formatGender, formatPrice } from '../../utils/formatters';
+import { buildFilterParams, makeRequest } from '../../utils/request';
 import './SalesTable.scss';
 
 const tableHeader = ['ID', 'Data', 'Gênero', 'Categoria', 'Loja', 'Forma de Pagamento', 'Total'];
 
-const SalesTable = () => {
+type Props = {
+  filterData?: FilterData;
+};
+
+const extraParams = {
+  page: 0,
+  size: 12,
+  sort: 'date,desc'
+};
+
+const SalesTable = ({ filterData }: Props) => {
+  const [sales, setSales] = useState<Sale[]>([]);
+
+  const params = useMemo(() => buildFilterParams(filterData, extraParams), [filterData]);
+
+  useEffect(() => {
+    makeRequest.get<SalesResponse>('/sales', { params }).then((response) => {
+      setSales(response.data.content);
+    });
+  }, [params]);
+
   return (
     <div className="sales-table-container base-card">
       <h3 className="sales-table-title">Vendas recentes</h3>
@@ -17,7 +41,17 @@ const SalesTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr></tr>
+          {sales.map((sale) => (
+            <tr key={sale.id}>
+              <td>#{sale.id}</td>
+              <td>{formatDate(sale.date)}</td>
+              <td>{formatGender(sale.gender)}</td>
+              <td>{sale.categoryName}</td>
+              <td>{sale.storeName}</td>
+              <td>{sale.paymentMethod}</td>
+              <td>{formatPrice(sale.total)}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
